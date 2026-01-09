4fe80a18c2ad3ea944553c92da6bea773f71e6d4
Muhammet Aydın <muhammetaydin1704@gmail.com>
2026-01-09 23:53:37 +0300

    feat:add maps screen

---
+++ lib/map.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+class MapScreen extends StatefulWidget {
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: LatLng(
+          51.509364,
+          -0.128928,
+        ), // Center the map over London, UK
+        initialZoom: 9.2,
+      ),
+      children: [
+        TileLayer(
+          // Bring your own tiles
+          urlTemplate:
+              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+          userAgentPackageName: "com.example.alarmbus",
+        ),
+      ],
+    );
+  }
+}
