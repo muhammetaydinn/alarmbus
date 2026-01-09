165336b3c594faade177b74ed243ef864dc0b86b
Muhammet Aydın <muhammetaydin1704@gmail.com>
2026-01-10 00:34:21 +0300

    feat: geolocation added

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.alarmbus"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
--- lib/map.dart
+++ lib/map.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:geolocator/geolocator.dart';
+
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
@@ -9,24 +11,82 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  Position? currentPosition;
   @override
   Widget build(BuildContext context) {
-    return FlutterMap(
-      options: MapOptions(
-        initialCenter: LatLng(
-          51.509364,
-          -0.128928,
-        ), // Center the map over London, UK
-        initialZoom: 9.2,
-      ),
-      children: [
-        TileLayer(
-          // Bring your own tiles
-          urlTemplate:
-              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
-          userAgentPackageName: "com.example.alarmbus",
+    print("currentPosition: $currentPosition");
+    return Scaffold(
+      body: FlutterMap(
+        mapController: MapController(),
+        options: MapOptions(
+          initialCenter: LatLng(
+            currentPosition?.latitude ?? 51.509364,
+            currentPosition?.longitude ?? -0.128928,
+          ), // Center the map over London, UK
+          initialZoom: 15,
         ),
-      ],
+        children: [
+          TileLayer(
+            // Bring your own tiles
+            urlTemplate:
+                'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+            userAgentPackageName: "com.example.alarmbus",
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // Action when button is pressed
+          print("currentPosition button pressed");
+          currentPosition = await _determinePosition();
+          setState(() {
+            currentPosition = currentPosition;
+          });
+        },
+        child: const Icon(Icons.my_location),
+      ),
     );
   }
+}
+
+/// Determine the current position of the device.
+///
+/// When the location services are not enabled or permissions
+/// are denied the `Future` will return an error.
+Future<Position> _determinePosition() async {
+  bool serviceEnabled;
+  LocationPermission permission;
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    // Location services are not enabled don't continue
+    // accessing the position and request users of the
+    // App to enable the location services.
+    return Future.error('Location services are disabled.');
+  }
+
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      // Permissions are denied, next time you could try
+      // requesting permissions again (this is also where
+      // Android's shouldShowRequestPermissionRationale
+      // returned true. According to Android guidelines
+      // your App should show an explanatory UI now.
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    // Permissions are denied forever, handle appropriately.
+    return Future.error(
+      'Location permissions are permanently denied, we cannot request permissions.',
+    );
+  }
+
+  // When we reach here, permissions are granted and we can
+  // continue accessing the position of the device.
+  return await Geolocator.getCurrentPosition();
 }
